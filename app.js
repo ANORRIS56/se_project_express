41a1f2c11999d1adba611da944ae0f6c4d3fba30
Andreas Norris <norrs.andrew00@gmail.com>
2026-04-28 22:45:59 -0400

    Project 13 complete - auth working

--- app.js
+++ app.js
@@ -5,16 +5,26 @@ const cors = require("cors");
 const routes = require("./routes");
 
 const app = express();
-
 const { PORT = 3001 } = process.env;
 
+// Connect to MongoDB
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133",
+  };
+  next();
+});
+
+// Routes
 app.use("/", routes);
 
+// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
